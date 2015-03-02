74c808c2a8bf6990a7f1889c456f87775f2b65bf
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2015-03-02 18:12:47 +0800

    add filters

--- angularjs/filters.js
+++ angularjs/filters.js
@@ -9,17 +9,30 @@
      * Avoid auto escape of html
      *
      * Usage:
-     * <div ng-bind-html="something_with_html_tags | sanitize"></div>
+     * <div ng-bind-html='something_with_html_tags | sanitize'></div>
      */
-    app.filter("sanitize", ['$sce', function($sce) {
+    app.filter('sanitize', ['$sce', function($sce) {
         return function(htmlCode) {
             return $sce.trustAsHtml(htmlCode);
         };
     }]);
 
-    app.filter("reverse", function() {
+    app.filter('reverse', function() {
        return function(text) {
-           return text.split("").reverse().join("");
+           return text.split('').reverse().join('');
        };
+    });
+    
+    app.filter('whitespace', function () {
+        return function(text) {
+            return text.replace('_', ' ');
+        };
+    });
+
+    app.filter('numberWithComma', function() {
+        return function(number) {
+            var text = number.toString();
+            return text.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+        };
     });
 })();
