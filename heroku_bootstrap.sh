5d5ca9b4bb392db06f3e0c0acf0f03379eda388e
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2015-06-25 13:31:33 +0800

    whitenoise

--- heroku_bootstrap.sh
+++ heroku_bootstrap.sh
@@ -2,8 +2,35 @@
 app=$(basename "$PWD")
 echo "DATABASE_URL=postgres://localhost/$app" > ./.env
 echo "web: gunicorn $app.wsgi --log-file -" > ./Procfile
-echo "For whitenoise static file serving:"
+echo "python-2.7.10" > runtime.txt
+echo "For whitenoise static file serving, follow the quick start:"
+echo "https://warehouse.python.org/project/whitenoise/"
+
 
 # output sample file content:
-echo "change wsgi.py"
-echo "change settings"
+echo "Change wsgi.py"
+echo "=============BGN=================="
+read -r -d '' VAR << EOM
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "$app.settings")
+
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
+EOM
+echo "$VAR"
+echo "=============END=================>"
+
+
+echo "Append settings.py:"
+echo "=============BGN================="
+read -r -d '' VAR << EOM
+STATIC_ROOT = 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+WSGI_APPLICATION = 'tagr.wsgi.application'
+ALLOWED_HOSTS = ["*"]
+EOM
+echo "$VAR"
+echo "=============END================="
